b3eb5ec1cde6a10ccdb81432e54364a3862ee4f8
leehyowon <dlgydnjs08@naver.com>
2019-12-04 15:14:34 +0900

    zZ

--- template1/reserve/reserve.php
+++ template1/reserve/reserve.php
@@ -1,10 +1,7 @@
 <?php
 include 'dbconnect.php';
 extract($_POST);
-<<<<<<< HEAD
 $id = $_POST["id"];
-=======
->>>>>>> bc1ab63bb94f62076b259d752ba7f025fdfdeb05
 $date = $_POST['date'];
 $name = $_POST['name'];
 $title = $_POST['title'];
@@ -16,11 +13,7 @@ foreach ($seat as $value) {
     $seat_val = $seat_val.",".$value;
 }
 
-<<<<<<< HEAD
 $sql = "INSERT INTO reservation_info (id,branch_name,title,movie_date,movie_time,seat,number,screen) VALUES ('".$id."','".$name."' ,'".$title."','".$date."','".$time."','".$seat_val."',$seat_num,'1관')";
-=======
-$sql = "INSERT INTO reservation_info (branch_name,title,movie_date,movie_time,seat,number,screen) VALUES ('".$name."' ,'".$title."','".$date."','".$time."','".$seat_val."',$seat_num,'1관')";
->>>>>>> bc1ab63bb94f62076b259d752ba7f025fdfdeb05
 $result = mysqli_query($dbconn, $sql);
 if($result){
     echo 1;
