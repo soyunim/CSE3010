7d794126157bdf33cfb739cf8bebd336a1a68957
JeongGod <jeonggyu@Adminui-MacBook-Air.local>
2019-12-04 16:48:14 +0900

    reserve

--- template1/reserve/reserve.php
+++ template1/reserve/reserve.php
@@ -1,11 +1,7 @@
 <?php
 include 'dbconnect.php';
 extract($_POST);
-<<<<<<< HEAD
 $id = $_POST['id'];
-=======
-$id = $_POST["id"];
->>>>>>> b3eb5ec1cde6a10ccdb81432e54364a3862ee4f8
 $date = $_POST['date'];
 $name = $_POST['name'];
 $title = $_POST['title'];
@@ -17,11 +13,7 @@ foreach ($seat as $value) {
     $seat_val = $value.",".$seat_val;
 }
 
-<<<<<<< HEAD
 $sql = "INSERT INTO reservation_info (branch_name,id,title,movie_date,movie_time,seat,number,screen) VALUES ('".$name."' ,'".$id."','".$title."','".$date."','".$time."','".$seat_val."',$seat_num,'1관')";
-=======
-$sql = "INSERT INTO reservation_info (id,branch_name,title,movie_date,movie_time,seat,number,screen) VALUES ('".$id."','".$name."' ,'".$title."','".$date."','".$time."','".$seat_val."',$seat_num,'1관')";
->>>>>>> b3eb5ec1cde6a10ccdb81432e54364a3862ee4f8
 $result = mysqli_query($dbconn, $sql);
 if($result){
     echo 1;
