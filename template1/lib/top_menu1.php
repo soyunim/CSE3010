cdf57dff55864cbf4ad11c582aafba9560a05de9
JeongGod <jeonggyu@Adminui-MacBook-Air.local>
2019-12-04 23:46:57 +0900

    hah

--- template1/lib/top_menu1.php
+++
@@ -1,7 +0,0 @@
-<div class="menus"><a href="./memo/memo.php"><img src="./img/home.gif"  width="140px" height ="38px" border="0"></a></div>
-<div class="menus"><a href="./greet/list.php"><img src="./img/booking.gif" width="140px" height ="38px"border="0"></a></div>
-<div class="menus"><a href="./concert/list.php"><img src="./img/ranking.gif" width="140px" height ="38px" border="0"></a></div>
-<div class="menus"><a href="./download/list.php"><img src="./img/review.gif" width="140px" height ="38px" border="0"></a></div>
-<div class="menus"><a href="./free/list.php"><img src="./img/expected.gif" width="140px" height ="38px" border="0"></a></div>
-<div class="menus"><a href="./qna/list.php"><img src="./img/worker_management.gif" width="140px" height ="38px"border="0"></a></div>
-<div class="menus"><a href="#"><img src="./img/sales_management.gif" width="140px" height ="38px"onclick="window.open('./survey/survey.php', '','scrollbars=no, toolbars=no,width=180,height=230')" border="0"></a></div>
