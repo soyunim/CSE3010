f6e1a28030334858e8a5c7114ffc703e38d3c268
JeongGod <jeonggyu@Adminui-MacBook-Air.local>
2019-12-04 23:37:52 +0900

    삭제

--- template1/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["userid"]);
-    unset($_SESSION["name"]);
-    unset($_SESSION["membership_rating"]);
-    header("Location:http://localhost:8888/template1/index.php");
- ?>
